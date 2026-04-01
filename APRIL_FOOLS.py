34b89442962eb5abc745e0a63d5fd38e77715717
mahibest <165934401+mahibest@users.noreply.github.com>
2026-04-01 16:47:51 +0400

    Enhance is_april_fools function with delete delay
    Added a delay before returning True when the delete button is pressed.

--- APRIL_FOOLS.py
+++ APRIL_FOOLS.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import time
+st.set_page_config("hacked")
 def is_april_fools():
     st.write("To fix the hacked computer you must delete whole os and restart to get a new fresh one.")
     if st.button("delete os and all its contents"):
+        st.write("deleting app")
+        time.wait(3)
         return True
     else:
         return False
